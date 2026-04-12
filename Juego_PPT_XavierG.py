1151bdca98eb270ddc56c2b8777eddb097915d26
Xavier Góngora <jogongorahi@uide.com>
2026-04-12 18:11:05 -0500

    1eraparte_xg

---
+++ Juego_PPT_XavierG.py
@@ -0,0 +1,25 @@
+import random #Para que el programa pueda generar numeros o listas aleatorios
+
+print("Piedra, papel o tijera") #Bienvenida del juego
+
+respuesta = input("\n¿Quieres jugar? (s/n): ") #Determino las opciones para jugar siendo s= sí y n= no
+jugar = respuesta.lower () == "s" #Variable jugar es s minuscula
+
+while jugar:
+    print("Elige: 1 = Piedra, 2 = Papel, 3 = Tijera") #determino las opciones que tiene el jugador
+    
+    jugador = input("Tu elección: ") #ingresa un número para elegir
+    
+    while jugador not in ["1", "2", "3"]: #si no es 1, 2 o 3 va dar error, evitamos eso con este while
+        print("Opción inválida, debes elegir 1, 2 o 3") #retorna el error del usuario
+        jugador = input("Vuelve a elegir: ") #para que pueda volver a elegir
+    
+    jugador = int(jugador) #ingresa como numero entero
+    print(f"Elegiste: {jugador}") #indica que eligió el jugador
+    pc= random.randint (1,3) #genera un número aleatorio la pc
+    if jugador == pc: #primer condicional, si son iguales da empate
+        print ("Empate")
+    
+    jugar = False
+
+print("Salir del juego")
